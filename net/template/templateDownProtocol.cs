8f7347d676059744e67ab5c6a3ee46434b422251
leeeyupeng <leeyupeng@126.com>
2015-07-06 12:59:00 +0800

    rm

--- net/template/templateDownProtocol.cs
+++
@@ -1,6 +0,0 @@
-    public static void OnRequest{name}(RakNet.BitStream bs)
-    {
-{data}
-
-        DownHandle{module}.OnRequest{name}({param});
-    }
--- net/template/templateUpProtocol.cs
+++
@@ -1,8 +0,0 @@
-    public static void Send{name}({param})
-    {
-        RakNet.BitStream bs = new RakNet.BitStream();
-
-{data}
-        Dispatcher.Instance.Send(MsgIDTypes.{msgID}, bs);
-    }
-
